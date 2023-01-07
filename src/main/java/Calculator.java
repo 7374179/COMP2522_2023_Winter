c5a8b6bfa11fe35d256f644f37aef7bbfde83325
taehyuk <7374179@gmail.com>
2023-01-07 01:43:01 -0800

    updates some methods

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,19 +1,30 @@
 /**
  * A class that defines common math functions.
- * @author TODO: put your name here
- * @version TODO: put the version and date here
+ *
+ * @author Taehyuk Chung
+ * @version 2023, January 6th
  */
 public class Calculator {
-    /**
-     * Adds two integers together.
-     *
-     * @param a integer input
-     * @param b integer input
-     * @return integer output
-     */
-    public int add(int a, int b) {
-        return a + b;
-    }
+  /**
+   * Adds two integers together.
+   *
+   * @param a integer input
+   * @param b integer input
+   * @return integer output
+   */
+  public int add(int a, int b) {
+    return a + b;
+  }
 
-    /* TODO: add overloaded methods to account for different types. */
+  public float add(float a, float b) {
+    return a + b;
+  }
+
+  public double add(double a, double b) {
+    return a + b;
+  }
+
+  public long add(long a, long b) {
+    return a + b;
+  }
 }
