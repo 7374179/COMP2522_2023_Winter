9b46e5ddd18b53a14c1225784f490bf971dcfeaa
Taehyuk <7374179@gmail.com>
2023-01-16 17:46:00 -0800

    complete daily assignment

--- src/main/java/org/bcit/comp2522/lectures/sl03/Letter.java
+++ src/main/java/org/bcit/comp2522/lectures/sl03/Letter.java
@@ -1,13 +1,28 @@
 package org.bcit.comp2522.lectures.sl03;
 
-public class Letter {
-  public static String order = "ZXEFTSRKPILGAMYBVUCQOWDNJH";
+import java.util.Arrays;
+
+public class Letter implements Comparable {
+//  public static String order = "ZXEFTSRKPILGAMYBVUCQOWDNJH";
+  public static String order = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+
   String val;
+
   public Letter(String val) {
     this.val = val;
   }
+
   public Letter(Character val) {
     this.val = val.toString();
   }
 
+  public int compareTo(Object o) {
+    if (((Letter) o).val.compareTo(this.val)>0){
+      return 1;
+    }else if(((Letter) o).val.compareTo(this.val)<0){
+      return -1;
+    }else {
+      return 0;
+    }
+  }
 }
